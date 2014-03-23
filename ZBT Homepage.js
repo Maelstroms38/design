2907edd60dbe90db72c5d74a24a881689058aba0
Michael Stromer <stroms38@gmail.com>
2014-03-23 12:22:56 -0400

    Create ZBT Homepage.js
    Online events javascript stylesheet

---
+++ ZBT Homepage.js
@@ -0,0 +1,45 @@
+h1 {text-align: center}
+
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;}
+ 
+form {
+margin:100;
+padding:100;
+text-align: center;}
+
+a.button {margin: 50px;
+padding: 10px;}
+
+button:hover{
+     background: #383;}
+
+button {color: #0088dd;
+border.width: thick;
+border-style: outset;
+border-radius: 10px;
+border-color: #0088dd;
+background-color: #0066FF;
+text-decoration: underline;
+font-color: #000000;
+font-family: "Lucida Grande", Geneva, Verdana, Arial, Helvetica, sans-serif;
+text-size:20px;
+font-size: 20px;
+box-shadow: 5px 5px 5px #777777;
+padding: 10px;
+height:36px;
+width: 250px;
+display: inline;
+margin: 40px;
+height: 50px;
+hover background-color: 0066FF;}
+
+menu {position: absolute;
+width:300px;
+height:200px;
+margin: 500px;
+z-index:15;
+top:50%;
+left:50%;}
